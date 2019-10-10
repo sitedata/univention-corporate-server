8251b2135ff00da4f4bf936c24d305c095fd9c5d
Florian Best <best@univention.de>
2019-10-10 20:09:32 +0200

    Bug #50347: do not reload extended attributes every time a attribute changes

--- services/univention-s4-connector/modules/univention/s4connector/__init__.py
+++ services/univention-s4-connector/modules/univention/s4connector/__init__.py
@@ -1159,11 +1159,6 @@ class ucs:
 					value = object['attributes'][attributes.ldap_attribute]
 					ud.debug(ud.LDAP, ud.INFO, '__set_values: set attribute, ucs_key: %s - value: %s' % (ucs_key, value))
 
-					ucs_module = self.modules[property_type]
-					position = univention.admin.uldap.position(self.lo.base)
-					position.setDn(object['dn'])
-					univention.admin.modules.init(self.lo, position, ucs_module)
-
 					if isinstance(value, type(types.ListType())) and len(value) == 1:
 						value = value[0]
 
@@ -1201,11 +1196,6 @@ class ucs:
 
 				ucs_key = attributes.ucs_attribute
 				if ucs_object.has_property(ucs_key):
-					ucs_module = self.modules[property_type]
-					position = univention.admin.uldap.position(self.lo.base)
-					position.setDn(object['dn'])
-					univention.admin.modules.init(self.lo, position, ucs_module)
-
 					# Special handling for con other attributes, see Bug #20599
 					if attributes.con_other_attribute:
 						if object['attributes'].get(attributes.con_other_attribute):
@@ -1260,6 +1250,12 @@ class ucs:
 
 	def add_in_ucs(self, property_type, object, module, position):
 		_d = ud.function('ldap.add_in_ucs')  # noqa: F841
+
+		# reload extended attributes  # FIXME: maybe not necessary
+		ucs_module = self.modules[property_type]
+		position = univention.admin.uldap.position(self.lo.base)
+		univention.admin.modules.init(self.lo, position, ucs_module)
+
 		ucs_object = module.object(None, self.lo, position=position)
 		if property_type == 'group':
 			ucs_object.open()
@@ -1267,6 +1263,7 @@ class ucs:
 			self.group_members_cache_ucs[object['dn'].lower()] = set()
 		else:
 			ucs_object.open()
+
 		self.__set_values(property_type, object, ucs_object, modtype='add')
 		for function in self.property[property_type].ucs_create_functions:
 			function(self, property_type, ucs_object)
@@ -1287,6 +1284,11 @@ class ucs:
 
 	def modify_in_ucs(self, property_type, object, module, position):
 		_d = ud.function('ldap.modify_in_ucs')  # noqa: F841
+
+		# reload extended attributes  # FIXME: maybe not necessary
+		ucs_module = self.modules[property_type]
+		position = univention.admin.uldap.position(self.lo.base)
+		univention.admin.modules.init(self.lo, position, ucs_module)
 
 		ucs_object_dn = object.get('olddn', object['dn'])
 		ucs_object = univention.admin.objects.get(module, None, self.lo, dn=ucs_object_dn, position='')
