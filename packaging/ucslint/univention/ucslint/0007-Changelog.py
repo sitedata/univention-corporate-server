ad56ac5f1d4d094e7a7a7feb239d2764c90736e4
Philipp Hahn <hahn@univention.de>
2020-09-08 09:14:43 +0200

    ucslint: Ignore debian/changelog error [TEMP]
    For now make monotonically increasing versions and time-stamps in
    debian/rules non-fatal.
    
    DO NOT APPLY TO FINAL UCS-5!

--- packaging/ucslint/univention/ucslint/0007-Changelog.py
+++ packaging/ucslint/univention/ucslint/0007-Changelog.py
@@ -51,8 +51,8 @@ class UniventionPackageCheck(uub.UniventionPackageCheckDebian):
 		return {
 			'0007-1': (uub.RESULT_WARN, 'failed to open file'),
 			'0007-2': (uub.RESULT_WARN, 'changelog does not contain ticket/bug/issue number'),
-			'0007-3': (uub.RESULT_ERROR, 'debian/changelog entries are not strict-monotonically increasing by time'),
-			'0007-4': (uub.RESULT_ERROR, 'debian/changelog entries are not strict-monotonically increasing by version'),
+			'0007-3': (uub.RESULT_WARN, 'debian/changelog entries are not strict-monotonically increasing by time'),
+			'0007-4': (uub.RESULT_WARN, 'debian/changelog entries are not strict-monotonically increasing by version'),
 			'0007-5': (uub.RESULT_WARN, 'old debian/changelog entries are not strict-monotonically increasing by time'),
 			'0007-6': (uub.RESULT_WARN, 'old debian/changelog entries are not strict-monotonically increasing by version'),
 		}
